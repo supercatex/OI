1e03cdda0da14cd2003074f1e6dc28999069bb37
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2021-03-31 15:20:51 +0800

    Update CJ2021_Q1.cpp

--- CJ2021_Q1.cpp
+++ CJ2021_Q1.cpp
@@ -1,3 +1,8 @@
+/*
+ * Author: Kinda Lam
+ * CodeJam 2021 Qualification -- Question 1 -- Reversort
+ * https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d0a5c
+ */
 #include <iostream>
 #include <vector>
 #include <algorithm>
